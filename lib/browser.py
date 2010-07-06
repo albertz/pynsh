1be14c2d63bced9b388bb455aeffee463c510702
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-07-06 20:07:59 +0200

    more demo

--- lib/browser.py
+++ lib/browser.py
@@ -1,13 +1,21 @@
 
 
-import sys
+import sys, webbrowser
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtWebKit import *
 
 app = QApplication(sys.argv)
 web = QWebView()
-web.load(QUrl("http://www.az2000.de"))
+
+web.load(QUrl("http://www.az2000.de/projects/javascript-project/"))
+web.page().setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
+
+
+def linkClicked(url): webbrowser.open(str(url.toString()))
+web.connect(web, SIGNAL("linkClicked (const QUrl&)"), linkClicked) 
+
+
 web.show()
 
 sys.exit(app.exec_())
