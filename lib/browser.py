2e0d84c7e4f43026389513039cdde43395dbf5a9
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2010-07-06 20:56:00 +0200

    more preparation

--- lib/browser.py
+++ lib/browser.py
@@ -8,14 +8,27 @@ from PyQt4.QtWebKit import *
 app = QApplication(sys.argv)
 web = QWebView()
 
-web.load(QUrl("http://www.az2000.de/projects/javascript-project/"))
+web.setHtml(file("html/main.html").read())
 web.page().setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
 
 
 def linkClicked(url): webbrowser.open(str(url.toString()))
 web.connect(web, SIGNAL("linkClicked (const QUrl&)"), linkClicked) 
 
-
 web.show()
+
+# testcode
+if False:
+	try:
+		from IPython.Shell import IPShellEmbed
+		ipshell = IPShellEmbed(argv=[''],banner="hello!",exit_msg="Goodbye")
+		ipshell()
+	except ImportError:
+		import code
+		# calling this with globals ensures we can see the environment
+		shell = code.InteractiveConsole(globals())
+		shell.interact()
+
+web.page().mainFrame().evaluateJavaScript("addHtml('<a href=http://www.az2000.de>www.az2000.de</a>')")
 
 sys.exit(app.exec_())
